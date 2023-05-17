0ffc11e60098a75cfc40ec19419f0a0d0cae6cc5
Ashton Bennett <ashtonbennett@ashtons-imac.lan>
2023-05-17 13:45:31 -0600

    adjusted layout for searching...

--- src/components/ProviderFinder.tsx
+++ src/components/ProviderFinder.tsx
@@ -153,7 +153,9 @@ const ProviderFinder: React.FC<providerProps> = ({
     <div className="max-w-screen-lg px-4">
       <ul className="pt-20">
         {noResultsFound && (
-          <h3>Sorry no providers in that area. Please try another.</h3>
+          <h3 className="h-48">
+            Sorry no providers in that area. Please try another.
+          </h3>
         )}
         {providers.length > 0 && typeof providers !== "string" ? (
           <>
@@ -221,16 +223,16 @@ const ProviderFinder: React.FC<providerProps> = ({
                         {" "}
                         <svg
                           fill="white"
-                          clip-rule="evenodd"
-                          fill-rule="evenodd"
-                          stroke-linejoin="round"
-                          stroke-miterlimit="2"
+                          clipRule="evenodd"
+                          fillRule="evenodd"
+                          strokeLinejoin="round"
+                          strokeMiterlimit="2"
                           viewBox="0 0 24 24"
                           xmlns="http://www.w3.org/2000/svg"
                         >
                           <path
                             d="m14.523 18.787s4.501-4.505 6.255-6.26c.146-.146.219-.338.219-.53s-.073-.383-.219-.53c-1.753-1.754-6.255-6.258-6.255-6.258-.144-.145-.334-.217-.524-.217-.193 0-.385.074-.532.221-.293.292-.295.766-.004 1.056l4.978 4.978h-14.692c-.414 0-.75.336-.75.75s.336.75.75.75h14.692l-4.979 4.979c-.289.289-.286.762.006 1.054.148.148.341.222.533.222.19 0 .378-.072.522-.215z"
-                            fill-rule="nonzero"
+                            fillRule="nonzero"
                           />
                         </svg>
                       </button>
@@ -247,8 +249,8 @@ const ProviderFinder: React.FC<providerProps> = ({
                             width="24"
                             height="24"
                             xmlns="http://www.w3.org/2000/svg"
-                            fill-rule="evenodd"
-                            clip-rule="evenodd"
+                            fillRule="evenodd"
+                            clipRule="evenodd"
                           >
                             <path d="M12.02 0c6.614.011 11.98 5.383 11.98 12 0 6.623-5.376 12-12 12-6.623 0-12-5.377-12-12 0-6.617 5.367-11.989 11.981-12h.039zm3.694 16h-7.427c.639 4.266 2.242 7 3.713 7 1.472 0 3.075-2.734 3.714-7m6.535 0h-5.523c-.426 2.985-1.321 5.402-2.485 6.771 3.669-.76 6.671-3.35 8.008-6.771m-14.974 0h-5.524c1.338 3.421 4.34 6.011 8.009 6.771-1.164-1.369-2.059-3.786-2.485-6.771m-.123-7h-5.736c-.331 1.166-.741 3.389 0 6h5.736c-.188-1.814-.215-3.925 0-6m8.691 0h-7.685c-.195 1.8-.225 3.927 0 6h7.685c.196-1.811.224-3.93 0-6m6.742 0h-5.736c.062.592.308 3.019 0 6h5.736c.741-2.612.331-4.835 0-6m-12.825-7.771c-3.669.76-6.671 3.35-8.009 6.771h5.524c.426-2.985 1.321-5.403 2.485-6.771m5.954 6.771c-.639-4.266-2.242-7-3.714-7-1.471 0-3.074 2.734-3.713 7h7.427zm-1.473-6.771c1.164 1.368 2.059 3.786 2.485 6.771h5.523c-1.337-3.421-4.339-6.011-8.008-6.771" />
                           </svg>
@@ -261,7 +263,7 @@ const ProviderFinder: React.FC<providerProps> = ({
             })}
           </>
         ) : noResultsFound ? null : (
-          "Searching..."
+          <div className="h-48">Searching...</div>
         )}
       </ul>
     </div>
